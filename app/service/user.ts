a2bcd81bbe452bc5b758d10187cf84c48d7dd95a
zhoujian <zhoujj-f@glodon.com>
2021-04-14 20:13:51 +0800

    feat(user.ts): 基础功能-服务

--- app/service/user.ts
+++ app/service/user.ts
@@ -1,6 +1,8 @@
 import { Service } from 'egg';
 export default class user extends Service {
-
+  // this.ctx.curl 发起网络调用。
+  // this.ctx.service.otherService 调用其他 Service。
+  // this.ctx.db 发起数据库调用等， db 可能是其他插件提前挂载到 app 上的模块。
 
   public async addUser(params:any) {
     console.log('===========经过复杂的处理=============');
@@ -9,7 +11,14 @@ export default class user extends Service {
 
   public async getUserInfo(id: string, name:string) {
     console.log('===========经过复杂的处理=============');
-    return { id, name };
+    const { ctx } = this;
+    const url = 'https://hooks.slack.com/services/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';
+    const result = await ctx.curl(url, {
+      method: 'POST',
+      contentType: 'json',
+      data: { text: 'Only test' },
+    });
+    return { id, name, result };
   }
 
   public async updadteUser(params:any) {
